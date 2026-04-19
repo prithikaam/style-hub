4641e63041f2921aa8b59c89e6638f39e104b1dd
prithikaam <prithikaam@gmail.com>
2026-04-19 15:07:38 +0530

    Fixed wildcard route for Express 5

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
   });
 }

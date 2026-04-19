ac61b9d657ae50f4c7a2dae8b4e8a2730ea76af5
prithikaam <prithikaam@gmail.com>
2026-04-19 14:37:00 +0530

    Added deployment setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 require('dotenv').config();
+const path = require("path");
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -23,6 +24,14 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
